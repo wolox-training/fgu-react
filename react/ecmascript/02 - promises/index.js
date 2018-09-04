ed4d1a8241c700eef3b915112ac596d695ec4e74
fedeGiussa25 <fedegiussa25@hotmail.com>
2018-09-04 12:38:12 -0300

    All tests passed

--- react/ecmascript/02 - promises/index.js
+++ react/ecmascript/02 - promises/index.js
@@ -24,6 +24,26 @@ return new Promise(
   )
 }
 
-export function asyncDelay() {
-
+export function asyncDelay(time) {
+  return delay(time);
 }
+/*  var start = Date.now();
+
+  return new Promise(
+      (resolve, reject) => {
+        if(time >= 400 && time <= 600) {
+          setTimeout(function(){
+            var timeLapse = Date.now() - start;
+            resolve(timeLapse);
+          }, time);
+        } else {
+          var reason;
+          if(time > 600){
+            reason = new Error('This time is too much !');
+          } else {
+            reason = new Error('This time is too short !');
+          }
+          reject(reason);
+        }
+      }
+    )*/
