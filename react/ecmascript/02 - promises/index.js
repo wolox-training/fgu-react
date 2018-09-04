6fe291498fc1de03a530033d862052ee06fe6e28
fedeGiussa25 <fedegiussa25@hotmail.com>
2018-09-04 11:57:31 -0300

    Test 1 / 02 - Promises

--- react/ecmascript/02 - promises/index.js
+++ react/ecmascript/02 - promises/index.js
@@ -1,7 +1,23 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
+export function delay(time) {
 
+var start = Date.now();
+
+return new Promise(
+    function (resolve, reject) {
+      if(time > 400 && time < 600) {
+        setTimeout(function(){
+          var timeLapse = Date.now() - start;
+          resolve(timeLapse);
+        }, time);
+        //setTimeout(resolve(Date.now() - start), time);
+      } else {
+        var reason = new Error('This time is too much !');
+        reject(reason);
+      }
+    }
+  )
 }
 
 export function asyncDelay() {
