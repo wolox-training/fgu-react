ad391e5901f4c67e8aaa9f30c6b047172b8ac307
fedeGiussa25 <fedegiussa25@hotmail.com>
2018-09-05 10:07:03 -0300

    Minor issues related to Promise tests solved

--- react/ecmascript/02 - promises/index.js
+++ react/ecmascript/02 - promises/index.js
@@ -1,4 +1,6 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
+const MIN_BASE_TIME = 400;
+const MAX_BASE_TIME = 600;
 
 export function delay(time) {
 
@@ -6,14 +8,11 @@ var start = Date.now();
 
 return new Promise(
     function (resolve, reject) {
-      if(time >= 400 && time <= 600) {
-        setTimeout(function(){
-          var timeLapse = Date.now() - start;
-          resolve(timeLapse);
-        }, time);
+      if(time >= MIN_BASE_TIME && time <= MAX_BASE_TIME) {
+        setTimeout(() => resolve(Date.now() - start), time);
       } else {
         var reason;
-        if(time > 600){
+        if(time > MAX_BASE_TIME){
           reason = new Error('This time is too much !');
         } else {
           reason = new Error('This time is too short !');
@@ -27,23 +26,3 @@ return new Promise(
 export function asyncDelay(time) {
   return delay(time);
 }
-/*  var start = Date.now();
-
-  return new Promise(
-      (resolve, reject) => {
-        if(time >= 400 && time <= 600) {
-          setTimeout(function(){
-            var timeLapse = Date.now() - start;
-            resolve(timeLapse);
-          }, time);
-        } else {
-          var reason;
-          if(time > 600){
-            reason = new Error('This time is too much !');
-          } else {
-            reason = new Error('This time is too short !');
-          }
-          reject(reason);
-        }
-      }
-    )*/
