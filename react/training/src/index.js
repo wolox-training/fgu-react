c943be9bc55cf74e5d48dd51c178385a407115c3
fedeGiussa25 <fedegiussa25@hotmail.com>
2018-09-07 18:31:14 -0300

    Changing location of the components files

--- react/training/src/index.js
+++ react/training/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Game from 'app/components/game';
+import Game from 'app/screens/Game/game';
 import './index.css';
 
 ReactDOM.render(<Game />, document.getElementById('root'));
