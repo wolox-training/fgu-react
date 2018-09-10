4b30616593d2200cd935cb3b7bf508ef303870bc
fedeGiussa25 <fedegiussa25@hotmail.com>
2018-09-10 15:32:46 -0300

    Functional Components

--- react/training/src/app/screens/Game/components/Square/index.js
+++ react/training/src/app/screens/Game/components/Square/index.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Square extends Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+export default function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
