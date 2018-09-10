4e163717603936dd39f70ede1c83785ef2968fda
fedeGiussa25 <fedegiussa25@hotmail.com>
2018-09-10 13:06:24 -0300

    Making an Interactive Component

--- react/training/src/app/screens/Game/components/Square/index.js
+++ react/training/src/app/screens/Game/components/Square/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 
 export default class Square extends Component {
+  state = { value: null };
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button className="square" onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value}
+      </button>
+    );
   }
 }
