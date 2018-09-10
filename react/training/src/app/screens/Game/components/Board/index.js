e678d2c45213fb3b95c945edb0931cbddc6e40a0
fedeGiussa25 <fedegiussa25@hotmail.com>
2018-09-10 16:31:48 -0300

    Declaring a Winner

--- react/training/src/app/screens/Game/components/Board/index.js
+++ react/training/src/app/screens/Game/components/Board/index.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 
 import Square from './../Square/index';
 
+function calculateWinner(squares) {
+  const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 export default class Board extends Component {
   state = { squares: Array(9).fill(null), xIsNext: true };
 
   handleClick(i) {
     const squaresCopy = this.state.squares.slice();
+    if (calculateWinner(squaresCopy) || squaresCopy[i]) {
+      return;
+    }
     squaresCopy[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({ squares: squaresCopy, xIsNext: !this.state.xIsNext });
   }
@@ -16,10 +30,16 @@ export default class Board extends Component {
   }
 
   render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
 
     return (
-      <div>
+      <React.Fragment>
         <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
@@ -36,7 +56,7 @@ export default class Board extends Component {
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
