1658b34103898fefd96ce00383a7e6165d727409
fedeGiussa25 <fedegiussa25@hotmail.com>
2018-09-10 17:46:50 -0300

    Showing the Past Moves

--- react/training/src/app/screens/Game/index.js
+++ react/training/src/app/screens/Game/index.js
@@ -26,7 +26,7 @@ export default class Game extends Component {
     this.setState({
       history: history.concat([
         {
-          squares: squares
+          squares
         }
       ]),
       xIsNext: !this.state.xIsNext
@@ -37,6 +37,15 @@ export default class Game extends Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? `Go to move #${move}` : 'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
 
     let status;
     if (winner) {
@@ -51,7 +60,7 @@ export default class Game extends Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
